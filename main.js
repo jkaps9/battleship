ad88b6fcc850188c706f552466cff08d011f0cb8
Josh <15219491+jkaps9@users.noreply.github.com>
2025-06-16 00:02:30 -0400

    feat: add HTML with two grids

---
+++ main.js
@@ -0,0 +1,6 @@
+// import { Player } from "./player";
+// import { Gameboard } from "./gameboard";
+// import { Ship } from "./ship";
+
+const playerOneBoard = document.querySelector("#player-one-board");
+const playerTwoBoard = document.querySelector("#player-two-board");
