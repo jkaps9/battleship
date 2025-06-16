2b93b302eaf7c3e89e835b9804f72204b12667ef
Josh <15219491+jkaps9@users.noreply.github.com>
2025-06-16 00:34:33 -0400

    feat: create boards with predetermined coords

--- game.js
+++ game.js
@@ -1,8 +1,46 @@
 import { Player } from "./player.js";
-import { Gameboard } from "./gameboard.js";
-import { Ship } from "./ship.js";
-import { DisplayController } from "./display-controller.js";
 
-const Game = function () {};
+const Game = function () {
+  const playerOne = new Player("human");
+  const playerTwo = new Player("computer");
+
+  const predeterminedGrid = function () {
+    const coords = [
+      {
+        startRow: 0,
+        startCol: 0,
+        endRow: 0,
+        endCol: 4,
+      },
+      {
+        startRow: 4,
+        startCol: 1,
+        endRow: 4,
+        endCol: 4,
+      },
+      {
+        startRow: 6,
+        startCol: 7,
+        endRow: 8,
+        endCol: 7,
+      },
+    ];
+
+    for (let i = 0; i < coords.length; i++) {
+      playerOne.gameboard.placeShip(
+        coords[i].startRow,
+        coords[i].startCol,
+        coords[i].endRow,
+        coords[i].endCol
+      );
+      playerTwo.gameboard.placeShip(
+        coords[i].startRow,
+        coords[i].startCol,
+        coords[i].endRow,
+        coords[i].endCol
+      );
+    }
+  };
+};
 
 export { Game };
