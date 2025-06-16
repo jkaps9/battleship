b6281596c566563d77325fe3519b6ea98de5837f
Josh <15219491+jkaps9@users.noreply.github.com>
2025-06-16 01:30:52 -0400

    feat: add event listeners

--- display-controller.js
+++ display-controller.js
@@ -4,6 +4,7 @@ import { Game } from "./game.js";
 class DisplayController {
   playerOneBoard = document.querySelector("#player-one-board");
   playerTwoBoard = document.querySelector("#player-two-board");
+  newGameButton = document.querySelector("#new-game");
 
   constructor() {
     this.game = new Game();
@@ -20,9 +21,14 @@ class DisplayController {
         const square = this.playerOneBoard.querySelector(
           `[data-row='${i}'][data-column='${j}']`
         );
+        square.className = "";
         if (p1board[i][j] == 0) {
         } else if (p1board[i][j] instanceof Ship) {
           square.classList.add("ship");
+        } else if (p1board[i][j] === "m") {
+          square.classList.add("miss");
+        } else if (p1board[i][j] === "x") {
+          square.classList.add("hit");
         }
       }
     }
@@ -32,15 +38,42 @@ class DisplayController {
         const square = this.playerTwoBoard.querySelector(
           `[data-row='${i}'][data-column='${j}']`
         );
-        if (p2board[i][j] == 0) {
+        if (p2board[i][j] === 0) {
         } else if (p2board[i][j] instanceof Ship) {
           square.classList.add("ship");
+        } else if (p2board[i][j] === "m") {
+          square.classList.add("miss");
+        } else if (p2board[i][j] === "x") {
+          square.classList.add("hit");
         }
       }
     }
   }
 
-  #addEventListeners() {}
+  #addEventListeners() {
+    this.newGameButton.addEventListener("click", () => {
+      this.game = new Game();
+      this.#displayBoards();
+    });
+
+    this.playerOneBoard.querySelectorAll("button").forEach((btn) => {
+      btn.addEventListener("click", () => {
+        let row = btn.getAttribute("data-row");
+        let column = btn.getAttribute("data-column");
+        this.game.playerOne.gameboard.receiveAttack(row, column);
+        this.#displayBoards();
+      });
+    });
+
+    this.playerTwoBoard.querySelectorAll("button").forEach((btn) => {
+      btn.addEventListener("click", () => {
+        let row = btn.getAttribute("data-row");
+        let column = btn.getAttribute("data-column");
+        this.game.playerTwo.gameboard.receiveAttack(row, column);
+        this.#displayBoards();
+      });
+    });
+  }
 }
 
 export { DisplayController };
