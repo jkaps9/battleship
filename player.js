ebeedcfdab4213cdf59b3c0c61f49c6f2d5d7728
Josh <15219491+jkaps9@users.noreply.github.com>
2025-06-15 23:41:00 -0400

    feat: add properties to player class

--- player.js
+++ player.js
@@ -1,3 +1,10 @@
-class Player {}
+import { Gameboard } from "./gameboard";
+
+class Player {
+  constructor(playerType) {
+    this.playerType = playerType;
+    this.gameboard = new Gameboard();
+  }
+}
 
 export { Player };
