bfe12685d6cc1cd995a0b150c30f776f2a217de3
Josh <15219491+jkaps9@users.noreply.github.com>
2025-06-15 23:02:18 -0400

    feat: placeShip function calculates ship size

--- gameboard.js
+++ gameboard.js
@@ -4,7 +4,8 @@ class Gameboard {
   grid = Array.from({ length: 10 }, () => new Array(10).fill(0));
 
   placeShip(rowStart, colStart, rowEnd, colEnd) {
-    let ship = new Ship();
+    let shipSize = rowEnd - rowStart + colEnd - colStart;
+    let ship = new Ship(shipSize);
     for (let i = rowStart; i <= rowEnd; i++) {
       for (let j = colStart; j <= colEnd; j++) {
         this.grid[i][j] = ship;
